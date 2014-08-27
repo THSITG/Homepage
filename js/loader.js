297642f1fe1e9ce0258d97573f6f80b3dcf7c876
CircuitCoder <circuitcoder0@gmail.com>
2014-08-27 13:18:37 +0800

    Finished directory structure Fixed navbar active slots not displayed properly

--- js/loader.js
+++ js/loader.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
-	var bar=$("#ph_navbar").load("/navbar.html");
-	var slot=bar.attr("data-active");
-	if(slot) {
-		bar.find(slot).addClass("active");
-	}
+	var bar=$("#ph_navbar").load("/navbar.html",function() {
+		var slot=bar.attr("data-active");
+		if(slot) {
+			bar.find("li:has(a[href='"+slot+"'])").addClass("active");
+		}
+	});
 
 	var foot=$("#ph_footer").load("/footer.html");
 });
